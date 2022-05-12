36d50280938d6c78f009a25b164a6e1181504c8b
Tomasmed18 <tomy-medina@hotmail.com>
2022-05-12 08:27:26 -0300

    Add Bean annotations to main application

--- alarmaApp/src/main/java/com/onboarding/alarmaapp/AlarmaAppApplication.java
+++ alarmaApp/src/main/java/com/onboarding/alarmaapp/AlarmaAppApplication.java
@@ -2,21 +2,39 @@ package com.onboarding.alarmaapp;
 
 import com.onboarding.central.CentralImpl;
 import org.alarma.common.Central;
+import org.alarma.common.Sensor;
 import org.example.sensores.SensorTemperatura;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class AlarmaAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AlarmaAppApplication.class, args);
-        Central central = new CentralImpl();
-        SensorTemperatura sensorTemperatura = new SensorTemperatura("1",central);
+        ConfigurableApplicationContext context = SpringApplication.run(AlarmaAppApplication.class, args);
+        Central central = context.getBean(Central.class);
+        Sensor sensorTemperatura = context.getBean(Sensor.class);
         sensorTemperatura.start();
 
-        SensorTemperatura sensorTemperatura2 = new SensorTemperatura("2",central);
+        Sensor sensorTemperatura2 = context.getBean(Sensor.class);
         sensorTemperatura2.start();
+    }
+
+    @Bean
+    public Central getCentral() {
+        return new CentralImpl();
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Sensor getSensor(Central central) {
+        String id = UUID.randomUUID().toString();
+        return new SensorTemperatura(id, central);
     }
 
 }
