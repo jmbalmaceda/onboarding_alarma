a016faa7ff57e7b4d5cf87db768c1789994f9965
veneremaia <venere.maia@gmail.com>
2022-06-23 08:43:54 -0300

    init palabra project

---
+++ palabra/src/main/java/com/onboarding/palabra/PalabraApplication.java
@@ -0,0 +1,13 @@
+package com.onboarding.palabra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalabraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalabraApplication.class, args);
+    }
+
+}
---
+++ palabra/src/main/java/com/onboarding/palabra/PalabraController.java
@@ -0,0 +1,19 @@
+package com.onboarding.palabra;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/palabra")
+public class PalabraController {
+
+    @GetMapping("{value}")
+    public ResponseEntity<String> change(@PathVariable(name = "value") String value){
+        String result = value + "_modificado";
+        return ResponseEntity.ok().body(result);
+    }
+}
