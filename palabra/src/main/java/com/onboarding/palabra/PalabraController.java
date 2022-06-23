a0fce85d753c8bc5cd633c976b3211b670dcd59b
veneremaia <venere.maia@gmail.com>
2022-06-23 09:16:51 -0300

    add async endpoint

--- palabra/src/main/java/com/onboarding/palabra/PalabraController.java
+++ palabra/src/main/java/com/onboarding/palabra/PalabraController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.concurrent.ForkJoinPool;
 
 @RestController
 @RequestMapping("/palabra")
@@ -16,5 +19,22 @@ public class PalabraController {
         Thread.sleep(5000);
         String result = value + "_modificado";
         return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping("/async-deferredresult/{value}")
+    public DeferredResult<ResponseEntity<?>> handleReqDefResult(@PathVariable(name = "value") String value) {
+        DeferredResult<ResponseEntity<?>> output = new DeferredResult<>();
+        
+        ForkJoinPool.commonPool().submit(() -> {
+            String result = value;
+            try {
+                Thread.sleep(5000);
+                result = value + "_modificado";
+            } catch (InterruptedException e) {
+            }
+            output.setResult(ResponseEntity.ok(result));
+        });
+
+        return output;
     }
 }
